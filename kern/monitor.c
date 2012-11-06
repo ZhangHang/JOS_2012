7a47629230ea6022bd58dd2fa0fc6bb8ada41614
zhanghang <zh199111@gmail.com>
2012-11-06 18:53:15 +0800

    实现 mon_backtrace 并加入到命令工具中

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display function stack one line at a time",mon_backtrace},
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -58,12 +59,19 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	uint32_t *ebp = (uint32_t*) read_ebp ();
+	uint32_t *ebp = (uint32_t*)read_ebp();
 
 	cprintf ("Stack backtrace:\n");
-	 while (ebp != 0x0) {
- 		cprintf (" ebp %08x eip %08x args %08x %08x %08x %08x %08x\n", ebp[0], ebp[1]
+ 	while (ebp != 0x0){
+ 		cprintf (" ebp %08x eip %08x args %08x %08x %08x %08x %08x\n", ebp, ebp[1]
                  , ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+	
+		struct Eipdebuginfo info;
+		debuginfo_eip(ebp[1],&info);	
+		cprintf ("%s:%d: %.*s+%d\n",info.eip_file
+					,info.eip_line
+					,info.eip_fn_namelen,info.eip_fn_name
+					,ebp[1]-info.eip_fn_addr);
  		ebp = (uint32_t*) ebp[0];
  	}
 ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼
